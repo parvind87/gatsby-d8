18821fbbecefe746e2aae1a3b6254441940951aa
PS <mail2parvind@gmail.com>
2020-02-23 23:42:55 +0530

    gatsby config updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        baseUrl: `http://d881.lan/`,
+        apiBase: `api`, // optional, defaults to `jsonapi`
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
